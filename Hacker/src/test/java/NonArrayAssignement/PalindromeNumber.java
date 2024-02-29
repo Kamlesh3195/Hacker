fd468c1c6e234f344cb416b30d49b1ce4bda6edd
Kamlesh3195 <157568114+Kamlesh3195@users.noreply.github.com>
2024-02-29 15:49:20 +0530

    add

---
+++ Hacker/src/test/java/NonArrayAssignement/PalindromeNumber.java
@@ -0,0 +1,28 @@
+package NonArrayAssignement;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
+public class PalindromeNumber {
+    //121
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number -- ");
+        int number = sc.nextInt();
+        int n=number;
+        int reverse =0;
+
+        while (number!=0){
+            int reminder = number%10;
+            reverse =reverse*10+reminder;
+            number = number/10;
+        }
+        if(n==reverse) {
+            System.out.println("Number is Palindrome");
+        }
+        else {
+            System.out.println("Number is not Palindrome");
+        }
+    }
+}
--- Hacker/src/test/java/NonArrayAssignement/fibonacciSeries.java
+++ Hacker/src/test/java/NonArrayAssignement/fibonacciSeries.java
@@ -1,5 +1,5 @@
 package NonArrayAssignement;
-
+//fibonacci Series -0 1 1 2 3 5 8 13 21 34 55 89 144
 public class fibonacciSeries {
     public static void main(String[] args) {
         int number = 10;
