3482b0009a6c9339e195d0aa2e93dde59cd48c27
Kamlesh3195 <157568114+Kamlesh3195@users.noreply.github.com>
2024-03-01 11:07:32 +0530

    add

--- Hacker/src/test/java/StringAssignement/vowelsAndConsonants.java
+++ Hacker/src/test/java/StringAssignement/vowelsAndConsonants.java
@@ -5,15 +5,30 @@ public class vowelsAndConsonants
     public static void main(String[] args)
     {
         String inputString = "Java concept of the day";
-        StringBuilder vowel = new StringBuilder();
-        StringBuilder Consonant = new StringBuilder();
+        String vowel=" ";
+        String consonant=" ";
+        int count = 0;
+        int total = 0;
         for(int i=0;i<inputString.length();i++)
         {
 
-            {
-
+            char ch = inputString.charAt(i);
+            if (ch!=' ') {
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    vowel = vowel+ " " + ch;
+                    count++;
+                } else {
+                    consonant = consonant+" " + ch;
+                    total++;
+                }
             }
         }
+
+        System.out.print("Total number of vowel--" + count + "--"+vowel);
+        System.out.println();
+        System.out.print("Total number of consonant--" + total + "--"+consonant);
+        System.out.println();
+        System.out.println(vowel+consonant);
 
     }
 }
